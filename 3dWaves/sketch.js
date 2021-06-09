463602323b10efd00b3916ced184808189264b0b
Filippo831 <filippoarduini832@gmail.com>
2021-06-09 21:34:26 +0200

    not working

--- 3dWaves/sketch.js
+++ 3dWaves/sketch.js
@@ -1,32 +1,31 @@
-
 let boxClass = class {
-  constructor(width, lenght,height) {
+  constructor(width, lenght, height) {
     this.width = width
-    this.height = height
+    this.lenght = lenght
     this.height = height
   }
   draw(value, xIndex, yIndex) {
-    translate(this.width*xIndex+.2, this.width*yIndex+.2)
-    fill(10*xIndex)
-    box(this.width, this.height)
+    //translate(this.width * xIndex + 0.2, this.lenght * yIndex + 0.2)
+    translate(1 * xIndex, 1 * yIndex )
+    box(this.width, this.lenght)
+    //box(5)
   }
 }
 
 let boxMatrix = []
 
-let yQuantity = 10
-let xQuantity = 10
+let yQuantity =2 
+let xQuantity =2 
 
 let totWidth = 400
-  let totLenght = 400
+let totLenght = 400
 let height = 100
 
 for (var xValue = 0, len = xQuantity; xValue < len; xValue++) {
   boxMatrix.push([])
   for (var yValue = 0, len = yQuantity; yValue < len; yValue++) {
     //boxMatrix.push(new boxClass(totWidth / xQuantity, totLenght / yQuantity, height))
-    boxMatrix[xValue].push(new boxClass(2,2,100))
-    
+    boxMatrix[xValue].push(new boxClass(2, 2, 20))
   }
 }
 
@@ -37,12 +36,10 @@ function setup() {
 
 function draw() {
   background(200)
-  orbitControl()
-  camera(1000,1000,0)
-  boxMatrix.forEach((x,xIndex) => {
+  orbitControl(2,2,2)
+  boxMatrix.forEach((x, xIndex) => {
     x.forEach((y, yIndex) => {
       y.draw(10, xIndex, yIndex)
     })
   })
-
 }
